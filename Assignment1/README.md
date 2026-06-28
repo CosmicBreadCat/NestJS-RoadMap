d3d98187f4ae48b643e4eff0724ad8a713503751
Adham Gaweesh <adhamgaweesh@gmail.com>
2026-06-28 16:54:48 +0300

    task one complete

---
+++ Assignment1/README.md
@@ -0,0 +1,32 @@
+Simple function which takes in an object and prints out a recipt string
+
+takes in an input object like so:
+
+```js
+const order = {
+  customerName: "Ahmad Ali",
+  quantity: "3",
+  unitPrice: "19.99",
+  discount: "5",
+  taxRate: "0.19",
+  shipping: "5",
+  status: "paid",
+};
+```
+
+and outputs a string like so:
+
+```
+Receipt:
+Customer: Ahmad Ali
+Subtotal: 59.97
+Discount: 5.00
+Tax: 10.44
+Shipping: 5.00
+Total: 70.41
+Status: paid
+```
+
+Notes:
+-added default values for all fields so the function runs even if some fields are missing
+-casting of all fields from string to numbers (because JS is an untyped mess :P)
--- Assignment1/index.js
+++ Assignment1/index.js
@@ -1,1 +1,29 @@
-console.log("Hello, World!");
+// test data
+const order = {
+  customerName: "Ahmad Ali",
+  quantity: "3",
+  unitPrice: "19.99",
+  discount: "5",
+  taxRate: "0.19",
+  shipping: "5",
+  status: "paid",
+};
+
+function generateReceipt(order) {
+  let subtotal = (+order.unitPrice ?? 20) * (+order.quantity ?? 1);
+  let discount = +order.discount ?? 0;
+  let tax = (subtotal - discount) * (+order.taxRate ?? 0.2);
+  let shipping = +order.shipping ?? 5;
+  const output = `
+  Receipt:
+    Customer: ${order.customerName ?? "John Doe"}
+    Subtotal: ${subtotal.toFixed(2)}
+    Discount: ${discount.toFixed(2)}
+    Tax: ${tax.toFixed(2)}
+    Shipping: ${shipping.toFixed(2)}
+    Total: ${(subtotal + tax + shipping - discount).toFixed(2)}
+    Status: ${order.status ?? "Not Paid"}`;
+  return output;
+}
+
+console.log(generateReceipt(order));
